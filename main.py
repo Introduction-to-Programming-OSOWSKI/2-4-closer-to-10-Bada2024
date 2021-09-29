878682e8a613993445c9e9fee6c751fad9952b81
Bada2024 <830561@mystma.org>
2021-09-28 22:44:57 -0500

    1st

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 #WRITE YOUR CODE IN THIS FILE
+def close10(x,y):
+    diffx= abs(x-10)
+    diffy=abs(y-10)
+    if diffx > diffy:
+        return y
+    elif diffx < diffy:
+        return x
+    else:
+        return 0
+print(close10(50,2000))
